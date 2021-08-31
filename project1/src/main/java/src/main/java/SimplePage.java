0b9798d2bdec3bb7e8d9d87c50f4bc60b2e5e0dd
bleedingwolf11 <56654525+bleedingwolf11@users.noreply.github.com>
2021-08-31 21:28:29 +0530

    Add files via upload

---
+++ project1/src/main/java/src/main/java/SimplePage.java
@@ -0,0 +1,18 @@
+package src.main.java;
+
+public class SimplePage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age=12,bd=12;
+		String name = "ff";
+		System.out.print("Name:");
+		System.out.print(name);
+		System.out.print("AGE:");
+		System.out.print(age);
+		System.out.print("Birth Date:");
+		System.out.print(bd);
+
+	}
+
+}
